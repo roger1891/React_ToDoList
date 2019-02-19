c02b3deda3413c54f550e25a1d4f8fba73ad7203
roger braun <rogerrainersbraun@gmail.com>
2019-02-20 02:49:34 +0800

    Initial Files

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+ function About(){
+	return (
+		<div>
+			<h1>About</h1>
+			<p>This is the TodoList app version 1.0.0. It is my frist react project.</p>
+		</div>
+	);
+}
+
+export default About;
